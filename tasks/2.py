4ff3b017e9df937e410e0bf19773ed0f6d5e797c
Ivan Shamaiski <shamaiskii.ivan@gmail.com>
2023-02-03 19:46:42 +0500

    second task have dove

---
+++ tasks/2.py
@@ -0,0 +1,15 @@
+"""
+Написать метод/функцию, который/которая на вход принимает число (float),
+а на выходе получает число, округленное до пятерок.
+"""
+
+
+def function_to_five(value, base):
+    five = base * round(value/base)
+    return five
+
+
+if __name__ == '__main__':
+    value = float(input('Введите рациональное число: '))
+    base = 5
+    print(function_to_five(value, base))
